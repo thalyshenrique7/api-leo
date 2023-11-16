5b145c1d4a4bdcc8e37cde9104a32e313a9a442d
thalyshenrique7 <thalys_henrique@outlook.com>
2023-11-16 19:11:21 -0300

    Alterações feitas na entidade Usuario.

--- src/main/java/com/dev/apileo/model/Usuario.java
+++ src/main/java/com/dev/apileo/model/Usuario.java
@@ -2,6 +2,7 @@ package com.dev.apileo.model;
 
 import java.util.Calendar;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -16,10 +17,13 @@ public class Usuario {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
+	@Column(nullable = false)
 	private String nome;
 
+	@Column(nullable = false)
 	private String cpf;
 
+	@Column(nullable = false)
 	private String rg;
 
 	private Calendar dataCriacao;
